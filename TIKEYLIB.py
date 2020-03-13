50c23134eabaededc9891f3023c3be1bfec8f74c
Paul Thorel <56446246+Paul-Thorel@users.noreply.github.com>
2020-03-13 21:28:18 +0100

    Added new functions and all keys ( no on ) support

--- TIKEYLIB.py
+++ TIKEYLIB.py
@@ -1,7 +1,58 @@
 from sys import *
+from time import *
 
-def getkey(n=3):
-  return list(stdin.readline(n))
+def chrput(c=0):
+  l=True
+  i=138
+  stdout.write(" ")
+  while l:
+    stdout.write("\b"+chr(i)+chr(131)+"\b")
+    g=getkey()
+    if g==" [A":
+      if i<255:
+        i+=1
+      if i==143:
+        i=162
+    elif g==" [B":
+      if i>=34:
+        i-=1
+      if i==161:
+        i=142
+    elif g==" [C":
+      l=False
+  if c==0:
+    stdout.write("\n")
+    return chr(i)
+  else:
+    return i
+
+def demo(tn=1):
+  t=monotonic()
+  l=""
+  while (t+tn)>monotonic():
+    l+=getkey()
+  print(len(l)/3,"touches appuyees en ",tn,"secondes")
+  return l
+
+def keyput(l):
+  s=""
+  c=""
+  for i in range(l):
+    c=getkey(1)
+    if c!="\b":
+      s+=c
+      stdout.write(s[i])
+    else:
+      s+=chr(ch(1))
+      stdout.write(s[i]+"\b")
+  stdout.write("\n")
+  return s
+
+def getkey(n=0):
+  if n==0:
+    return stdin.read(3).replace(chr(27)," ")
+  else:
+    return stdin.read(1)
 
 def ispressed(key="up",keylist=[]):
   if key=="enter":
@@ -17,18 +68,10 @@ def ispressed(key="up",keylist=[]):
   if key=="annul":
     key=" [2"
 
-  for i in range(3):
-    if key[i]==keylist[i]:
-      p=True
-    else:
-      p=False
-  return p
-
-
-
-
-
-
+  if key==keylist:
+    return True
+  else:
+    return False
 
 """use like this :
 g=getkey()
